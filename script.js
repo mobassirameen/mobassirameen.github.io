b3726b93681d76f9c143ad92f76db52f7ecaf349
Mohammad Mobassir Ameen <76399971+mobassirameen@users.noreply.github.com>
2026-08-13 10:45:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -231,3 +231,14 @@ if (footer) {
         `© ${new Date().getFullYear()} Mohammad Mobassir Ameen`;
 
 }
+
+
+// ==========================================
+// Email clickable
+// ==========================================
+
+<p class="email-copy" onclick="copyEmail()">
+    <i class="fas fa-envelope"></i>
+    <span id="email">mobassir.ameen7866@email.com</span>
+    <i class="fas fa-copy copy-icon"></i>
+</p>
